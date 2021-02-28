842d0fe6ab8d6e6232a93616603fb996fb81ee0e
cwtowns <github@farmerhodge.com>
2021-02-28 13:16:43 -0600

    moved type registration to extension method (#8)
    Co-authored-by: Chris Townsend <cwtowns77@gmail.com>

--- Collier/Program.cs
+++ Collier/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Reflection;
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Serilog;
@@ -11,28 +12,6 @@ namespace GrpcGreeter
 {
     public class Program
     {
-        //TODO can I keep color output?  I dislike my log right now and color output would be very nice.
-
-        //TODO can it automatically pause when a process is detected?  that could be cool.  It doesn't look like this takes a lot of resources at all.
-        //  There could be an auto mode and an override to manually pause.  When would I manually pause?  Almost never I would think, and I could auto unpause if I go idle for 20m.
-        //  this would be way more efficient which would be cool. 
-
-        /*
-        //  service startup 
-        //      at some point I will want to take over a process, this will require shutting it down gracefully via http
-        //     
-        //  gpu monitor will poll for running apps and fire an event if they are running 
-        //  if no apps are running and load is low then we unpause.  this exists today.
-        
-        //  need another thing that looks for a gaming process and pauses the miner.
-        //  
-        //
-        */
-
-        //TODO add a feature that lets us specify what miner output we show at informational level so we can turn off some of the log spam 
-        //this is probably going to be the rows that show hash rate and that's it?
-        //ultimately my client is going to want to also stream log info from the entire application / get status remotely
-
         //TODO how does running as a service work?
         //https://mcilis.medium.com/how-to-publish-net-core-grpc-server-as-a-windows-service-dd562a1e263d
         //puslishing models
@@ -57,20 +36,15 @@ namespace GrpcGreeter
         //C:\Windows\System32\config\systemprofile\AppData\Local\Collier\log\
         //that is very difficult to figure out (had to use procmon)
 
-
-
         //creating the installer using WIX
         //https://nblumhardt.com/2017/04/netcore-msi/
         //once I set this up I can run the dev platform with a fake process that acts like the miner so I can iterate on this
         //while still mining via my installed service
 
-        internal static string ENV_VARIABLE_COLLIER_ROOT_DIRECTORY = "COLLIER_ROOT_DIRECTORY";
-
         public static void Main(string[] args)
         {
             var rootDirectory = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
-
-            Environment.SetEnvironmentVariable(ENV_VARIABLE_COLLIER_ROOT_DIRECTORY, rootDirectory);
+            Environment.SetEnvironmentVariable(CollierServiceCollectionExtensions.ENV_VARIABLE_COLLIER_ROOT_DIRECTORY, rootDirectory);
 
             CreateHostBuilder(args).Build().Run();
         }
--- Collier/Startup.cs
+++ Collier/Startup.cs
@@ -28,9 +28,8 @@ namespace GrpcGreeter
     {
         private IServiceCollection _services;
         private IConfiguration _configuration;
-        private IWebHostEnvironment _hostEnv;
 
-        private CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+        private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
 
         // This method gets called by the runtime. Use this method to add _services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
@@ -38,8 +37,8 @@ namespace GrpcGreeter
         {
             _services = services;
 
-            var rootDirectory = Environment.GetEnvironmentVariable(Program.ENV_VARIABLE_COLLIER_ROOT_DIRECTORY) ??
-                                throw new ArgumentNullException("env." + Program.ENV_VARIABLE_COLLIER_ROOT_DIRECTORY,
+            var rootDirectory = Environment.GetEnvironmentVariable(CollierServiceCollectionExtensions.ENV_VARIABLE_COLLIER_ROOT_DIRECTORY) ??
+                                throw new ArgumentNullException("env." + CollierServiceCollectionExtensions.ENV_VARIABLE_COLLIER_ROOT_DIRECTORY,
                                     "This is normally set during Main execution.");
 
             _configuration = new ConfigurationBuilder()
@@ -49,82 +48,8 @@ namespace GrpcGreeter
                 .AddEnvironmentVariables()
                 .Build();
 
-
             _services.AddGrpc();
-
-            //TODO long term look for a framework that automates this binding.  this is a lot of boilerplate code
-
-            //_services.Configure<GpuMonitoringSettings>(_configuration.GetSection("gpuMonitoring"));
-
-            var monitoringSection = _configuration.GetSection("monitoring");
-            var tRexMinerSection = _configuration.GetSection("miner").GetSection("t-rex");
-
-            _services.Configure<TrexMiner.Settings>(options => tRexMinerSection.Bind(options));
-            _services.Configure<TrexWebClient.Settings>(options => tRexMinerSection.GetSection("web").Bind(options));
-
-
-            var gpuMonitoring = monitoringSection.GetSection("gpuMonitoring");
-
-            _services.Configure<Collier.Monitoring.Gpu.GpuMonitoringBackgroundService.Settings>(options => gpuMonitoring.Bind(options));
-
-            _services.Configure<GpuMonitorOutputParser_ProcessList.Settings>(options => gpuMonitoring.GetSection("outputParsers").GetSection("processListOutputParser").Bind(options));
-
-            _services.Configure<NvidiaSmiExecutor.Settings>(options => gpuMonitoring.Bind(options));
-
-
-            _services.Configure<IdleMonitor.Settings>(options => monitoringSection.GetSection("userIdle").Bind(options));
-            _services.Configure<IdleMonitorBackgroundService.Settings>(options => monitoringSection.GetSection("userIdle").Bind(options));
-
-
-            //TODO refactor the DI wire up to follow dot net core best practices via extension methods
-
-            _services.AddSingleton<IMiner, TrexMiner>();
-            _services.AddSingleton<IGpuMonitorOutputParser, GpuMonitorOutputParser_ProcessList>();
-            _services.AddSingleton<ITrexWebClient, TrexWebClient>();
-            _services.AddSingleton<IGpuMonitoringBackgroundService, Collier.Monitoring.Gpu.GpuMonitoringBackgroundService>();
-            _services.AddSingleton<IGpuProcessMonitor<GpuProcessEvent>, GpuMonitorOutputParser_ProcessList>();
-            _services.AddSingleton<IIdleMonitorBackgroundService, IdleMonitorBackgroundService>();
-            _services.AddSingleton<IIdleMonitor, IdleMonitor>();
-            _services.AddSingleton<ProcessFactory, ProcessFactory>();
-            _services.AddSingleton<INvidiaSmiParser, NvidiaSmiParser>();
-            _services.AddSingleton<INvidiaSmiExecutor, NvidiaSmiExecutor>();
-            _services.AddSingleton<ITrexLogModifier, DateStrippingTrexLogModifier>();
-
-            _services.AddSingleton<IGpuMonitor, GpuMonitor>();
-            _services.AddSingleton(new HttpClient());
-
-            //_services.AddSingleton<IBackgroundService<GpuMonitoringBackgroundService>, GpuMonitoringBackgroundService>();
-            //_services.AddSingleton<IBackgroundService<IdleMonitorBackgroundService>, IdleMonitorBackgroundService>();
-            //_services.AddSingleton<IBackgroundService<EventCoordinatorBackgroundService>, EventCoordinatorBackgroundService>();
-
-            _services.AddSingleton<IMinerProcessFactory, MinerProcessFactory>();
-            _services.AddSingleton<IApplicationCancellationTokenFactory>(new DefaultCancellationTokenFactory(_cancellationTokenSource.Token));
-
-            _services.AddHostedService<Collier.Host.GpuMonitoringBackgroundService>();
-            //_services.AddHostedService<Collier.Host.IdleMonitoringBackgroundService>();
-            //_services.AddHostedService<Collier.Host.EventCoordinatorBackgroundService>();
-
-            //TODO why doesnt this approach work?  Make a sample project and post on stack overflow?
-
-            //_services.AddHostedService<BackgroundServiceHelper<GpuMonitoringBackgroundService>>();
-            //_services.AddHostedService<BackgroundServiceHelper<IdleMonitorBackgroundService>>();
-            //_services.AddHostedService<BackgroundServiceHelper<EventCoordinatorBackgroundService>>();
-            /*
-             *      //_services.AddHostedService<GpuMonitoringBackgroundService>();
-             *      //_services.AddHostedService<IdleMonitorBackgroundService>();
-             *      
-             *      If we register the _services themselves with the DI container as singletons types
-             *      and also as _services, we will not get singleton behavior.  Apparently
-             *      we have to delegate control of the sub _services to the parent service and only
-             *      add the parent service as a hosted service.  
-             *      
-             *      https://github.com/dotnet/extensions/issues/553#issuecomment-462892616
-             *      
-             *      basically says do not depend on the service itself, depend on a different abstraction
-             *      this means event Coordinator background service needs to depend on something else for GPU and Idle background service.
-             *      
-             *      //TODO remove the weirdness of how I'm doing background service injection
-             */
+            _services.AddCollier(_configuration, _cancellationTokenSource);
 
             Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(_configuration).CreateLogger();
         }
@@ -137,11 +62,6 @@ namespace GrpcGreeter
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, Microsoft.Extensions.Hosting.IHostApplicationLifetime lifetime)
         {
-
-            _hostEnv = env;
-
-            var cancellationSource = new CancellationTokenSource();
-
             lifetime.ApplicationStopping.Register(OnShutdown);
 
             if (env.IsDevelopment())
