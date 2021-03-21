2d94d19cffce9ad05866e0e06bcde2de535787c1
cwtowns <github@farmerhodge.com>
2021-03-21 12:51:37 -0500

    inital work (#14)
    Co-authored-by: Chris Townsend <cwtowns77@gmail.com>

---
+++ CollierTests/Mining/MinerEventStateTests.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Collier.Host;
+using Collier.IO;
+using Collier.Mining;
+using Collier.Mining.OutputParsing;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using Xunit;
+
+namespace CollierTests.Mining
+{
+    public class MinerEventStateTests
+    {
+        [Fact]
+        public void DisposedMinerFiresStop()
+        {
+            var mockWebClient = new Mock<ITrexWebClient>();
+
+            var logger = new Mock<ILogger<TrexMiner>>();
+            var settings = new TrexMiner.Settings();
+            var factory = new Mock<IMinerProcessFactory>();
+            var mockLogListener = new Mock<IMinerLogListener>();
+            var mockLogObserver = new Mock<IInternalLoggingFrameworkObserver>();
+
+            var miner = new TrexMiner(logger.Object, Options.Create(settings), mockWebClient.Object, factory.Object, mockLogListener.Object, mockLogObserver.Object);
+            var statePassed = false;
+
+            miner.MiningInformationChanged += (o, e) =>
+            {
+                statePassed = e.Value == IMiner.MiningState.Stopped.ToString();
+            };
+
+            miner.Dispose();
+            statePassed.Should().Be(true, "When we dispose of the miner we should always indicate a stopped state.");
+        }
+
+        [Fact]
+        public async void StoppedMinerFiresStopWhenItsNotRunning()
+        {
+            var mockWebClient = new Mock<ITrexWebClient>();
+
+            var logger = new Mock<ILogger<TrexMiner>>();
+            var settings = new TrexMiner.Settings();
+            var factory = new Mock<IMinerProcessFactory>();
+            var mockLogListener = new Mock<IMinerLogListener>();
+            var mockLogObserver = new Mock<IInternalLoggingFrameworkObserver>();
+
+            var miner = new TrexMiner(logger.Object, Options.Create(settings), mockWebClient.Object, factory.Object, mockLogListener.Object, mockLogObserver.Object);
+            var statePassed = false;
+
+            miner.MiningInformationChanged += (o, e) =>
+            {
+                statePassed = e.Value == IMiner.MiningState.Stopped.ToString();
+            };
+
+            await miner.Stop();
+            statePassed.Should().Be(true, "When we stop the miner we should always indicate a stopped state.");
+        }
+
+        [Fact]
+        public async void StoppedMinerFiresStopWhenItsKilled()
+        {
+            var mockWebClient = new Mock<ITrexWebClient>();
+
+            var logger = new Mock<ILogger<TrexMiner>>();
+            var settings = new TrexMiner.Settings();
+            var factory = new Mock<IMinerProcessFactory>();
+
+            var process = new Mock<IProcess>();
+            process.Setup(x => x.HasExited).Returns(false);
+            factory.Setup(x => x.CurrentProcess).Returns(process.Object);
+
+            var mockLogListener = new Mock<IMinerLogListener>();
+            var mockLogObserver = new Mock<IInternalLoggingFrameworkObserver>();
+
+            var miner = new TrexMiner(logger.Object, Options.Create(settings), mockWebClient.Object, factory.Object, mockLogListener.Object, mockLogObserver.Object);
+            var statePassed = false;
+
+            miner.MiningInformationChanged += (o, e) =>
+            {
+                statePassed = e.Value == IMiner.MiningState.Stopped.ToString();
+            };
+
+            await miner.Stop();
+            statePassed.Should().Be(true, "When we stop the miner we should always indicate a stopped state.");
+        }
+
+        [Fact]
+        public async void RunningMinerFiresStateWhenProcessIsRunning()
+        {
+            var mockWebClient = new Mock<ITrexWebClient>();
+
+            var logger = new Mock<ILogger<TrexMiner>>();
+            var settings = new TrexMiner.Settings();
+            var factory = new Mock<IMinerProcessFactory>();
+
+            var process = new Mock<IProcess>();
+            process.Setup(x => x.HasExited).Returns(false);
+            factory.Setup(x => x.CurrentProcess).Returns(process.Object);
+
+            var mockLogListener = new Mock<IMinerLogListener>();
+            var mockLogObserver = new Mock<IInternalLoggingFrameworkObserver>();
+
+            var miner = new TrexMiner(logger.Object, Options.Create(settings), mockWebClient.Object, factory.Object, mockLogListener.Object, mockLogObserver.Object);
+            var statePassed = false;
+
+            miner.MiningInformationChanged += (o, e) =>
+            {
+                statePassed = e.Value == IMiner.MiningState.Running.ToString();
+            };
+
+            await miner.IsRunningAsync();
+            statePassed.Should().Be(true, "When we fire the runing event when the process is running.");
+        }
+
+        [Fact]
+        public async void RunningMinerFiresStateWhenProcessIsNotRunningButWebClientIs()
+        {
+            var mockWebClient = new Mock<ITrexWebClient>();
+
+            mockWebClient.Setup(x => x.IsMiningAsync()).ReturnsAsync(true);
+
+            var logger = new Mock<ILogger<TrexMiner>>();
+            var settings = new TrexMiner.Settings();
+            var factory = new Mock<IMinerProcessFactory>();
+
+            var process = new Mock<IProcess>();
+            process.Setup(x => x.HasExited).Returns(true);
+            factory.Setup(x => x.CurrentProcess).Returns(process.Object);
+
+            var mockLogListener = new Mock<IMinerLogListener>();
+            var mockLogObserver = new Mock<IInternalLoggingFrameworkObserver>();
+
+            var miner = new TrexMiner(logger.Object, Options.Create(settings), mockWebClient.Object, factory.Object, mockLogListener.Object, mockLogObserver.Object);
+            var statePassed = false;
+
+            miner.MiningInformationChanged += (o, e) =>
+            {
+                statePassed = e.Value == IMiner.MiningState.Running.ToString();
+            };
+
+            await miner.IsRunningAsync();
+            statePassed.Should().Be(true, "When we fire the runing event when the web server is running.");
+        }
+
+        [Fact]
+        public async void RunningMinerFiresStateWhenNothingIsRunning()
+        {
+            var mockWebClient = new Mock<ITrexWebClient>();
+
+            mockWebClient.Setup(x => x.IsMiningAsync()).ReturnsAsync(true);
+
+            var logger = new Mock<ILogger<TrexMiner>>();
+            var settings = new TrexMiner.Settings();
+            var factory = new Mock<IMinerProcessFactory>();
+
+            var process = new Mock<IProcess>();
+            process.Setup(x => x.HasExited).Returns(true);
+            factory.Setup(x => x.CurrentProcess).Returns(process.Object);
+
+            var mockLogListener = new Mock<IMinerLogListener>();
+            var mockLogObserver = new Mock<IInternalLoggingFrameworkObserver>();
+
+            var miner = new TrexMiner(logger.Object, Options.Create(settings), mockWebClient.Object, factory.Object, mockLogListener.Object, mockLogObserver.Object);
+            var statePassed = false;
+
+            miner.MiningInformationChanged += (o, e) =>
+            {
+                statePassed = e.Value == IMiner.MiningState.Running.ToString();
+            };
+            await miner.IsRunningAsync();
+            statePassed.Should().Be(true, "When we fire the runing event when the web server is running.");
+        }
+
+
+        [Fact]
+        public async void StartingMinerFiresStateWhenMining()
+        {
+            var mockWebClient = new Mock<ITrexWebClient>();
+
+            mockWebClient.Setup(x => x.IsRunningAsync()).ReturnsAsync(true);
+            mockWebClient.Setup(x => x.IsMiningAsync()).ReturnsAsync(true);
+
+            var logger = new Mock<ILogger<TrexMiner>>();
+            var settings = new TrexMiner.Settings();
+            var factory = new Mock<IMinerProcessFactory>();
+
+            var process = new Mock<IProcess>();
+            process.Setup(x => x.HasExited).Returns(false);
+            factory.Setup(x => x.CurrentProcess).Returns(process.Object);
+
+            var mockLogListener = new Mock<IMinerLogListener>();
+            var mockLogObserver = new Mock<IInternalLoggingFrameworkObserver>();
+
+            var miner = new TrexMiner(logger.Object, Options.Create(settings), mockWebClient.Object, factory.Object, mockLogListener.Object, mockLogObserver.Object);
+            var statePassed = false;
+
+            miner.MiningInformationChanged += (o, e) =>
+            {
+                statePassed = e.Value == IMiner.MiningState.Running.ToString();
+            };
+
+            await miner.Start();
+            statePassed.Should().Be(true, "When we fire the runing event when the web server is up and mining.");
+        }
+
+        [Fact]
+        public async void StartingMinerFiresStateWhenPaused()
+        {
+            var mockWebClient = new Mock<ITrexWebClient>();
+
+            mockWebClient.Setup(x => x.IsRunningAsync()).ReturnsAsync(true);
+            mockWebClient.Setup(x => x.IsMiningAsync()).ReturnsAsync(false);
+
+            var logger = new Mock<ILogger<TrexMiner>>();
+            var settings = new TrexMiner.Settings();
+            var factory = new Mock<IMinerProcessFactory>();
+
+            var process = new Mock<IProcess>();
+            process.Setup(x => x.HasExited).Returns(false);
+            factory.Setup(x => x.CurrentProcess).Returns(process.Object);
+
+            var mockLogListener = new Mock<IMinerLogListener>();
+            var mockLogObserver = new Mock<IInternalLoggingFrameworkObserver>();
+
+            var miner = new TrexMiner(logger.Object, Options.Create(settings), mockWebClient.Object, factory.Object, mockLogListener.Object, mockLogObserver.Object);
+            var statePassed = false;
+
+            miner.MiningInformationChanged += (o, e) =>
+            {
+                statePassed = e.Value == IMiner.MiningState.Paused.ToString();
+            };
+
+            await miner.Start();
+            statePassed.Should().Be(true, "We should fire the paused event when the web server is up but not mining.");
+        }
+
+    }
+}
--- CollierTests/Mining/TrexMinerTests.cs
+++ CollierTests/Mining/TrexMinerTests.cs
@@ -11,9 +11,6 @@ namespace CollierTests.Mining
 {
     public class TrexMinerTests
     {
-        private static readonly ILogger<InternalLoggingFrameworkObserver> logObserver =
-            new Mock<ILogger<InternalLoggingFrameworkObserver>>().Object;
-
         [Fact]
         public async void WebMinerIsCheckedWhenProcessIsDead()
         {
@@ -35,6 +32,30 @@ namespace CollierTests.Mining
         }
 
         [Fact]
+        public void DisposedMinerKillsProcessIfRunning()
+        {
+            var mockWebClient = new Mock<ITrexWebClient>();
+
+            var logger = new Mock<ILogger<TrexMiner>>();
+            var settings = new TrexMiner.Settings();
+            var factory = new Mock<IMinerProcessFactory>();
+
+            var mockProcess = new Mock<IProcess>();
+            mockProcess.Setup(x => x.HasExited).Returns(false);
+
+            factory.Setup(x => x.CurrentProcess).Returns(mockProcess.Object);
+
+            var mockLogListener = new Mock<IMinerLogListener>();
+            var mockLogObserver = new Mock<IInternalLoggingFrameworkObserver>();
+
+            var miner = new TrexMiner(logger.Object, Options.Create(settings), mockWebClient.Object, factory.Object, mockLogListener.Object, mockLogObserver.Object);
+
+            miner.Dispose();
+
+            mockProcess.Verify(x => x.Kill(It.IsAny<bool>()), Times.Once, "When the object id disposed and the process is running we should attempt to kill it.");
+        }
+
+        [Fact]
         public async void WebMinerIsInvokedWhenProcessIsRunning()
         {
             var methodCalled = false;
@@ -53,13 +74,13 @@ namespace CollierTests.Mining
 
             var miner = new TrexMiner(logger.Object, Options.Create(settings), mockWebClient.Object, factory.Object, mockLogListener.Object, mockLogObserver.Object);
 
-            miner.Start();
+            await miner.Start();
             await miner.IsRunningAsync();
 
             methodCalled.Should().Be(true, "we should invoke the web client when the process is running.");
         }
         [Fact]
-        public void StopKillsWhenTheProcessIsStillRunning()
+        public async void StopKillsWhenTheProcessIsStillRunning()
         {
             var methodCalled = false;
             var mockWebClient = new Mock<ITrexWebClient>();
@@ -90,15 +111,15 @@ namespace CollierTests.Mining
 
             var miner = new TrexMiner(logger.Object, Options.Create(settings), mockWebClient.Object, factory.Object, mockLogListener.Object, mockLogObserver.Object);
 
-            miner.Start();
-            miner.Stop();
+            await miner.Start();
+            await miner.Stop();
 
             //this is because pausing t-rex doesn't free memory.  The DAG persists and that kills gaming performance
             methodCalled.Should().Be(true, "we kill the web client when the process is running.");
         }
 
         [Fact]
-        public void StartDoesNotCallResumeWhenTheProcessIsStillRunningButMinerNotInitialized()
+        public async void StartDoesNotCallResumeWhenTheProcessIsStillRunningButMinerNotInitialized()
         {
             var methodCallCount = 0;
             var mockWebClient = new Mock<ITrexWebClient>();
@@ -131,14 +152,14 @@ namespace CollierTests.Mining
 
             var miner = new TrexMiner(logger.Object, Options.Create(settings), mockWebClient.Object, factory.Object, mockLogListener.Object, mockLogObserver.Object);
 
-            miner.Start();
-            miner.Start();
+            await miner.Start();
+            await miner.Start();
 
             methodCallCount.Should().Be(0, "we should not ask the web client to resume when the web client appears unresponsive");
         }
 
         [Fact]
-        public void StartCallsResumeOnlyWhenProcessIsResponding()
+        public async void StartCallsResumeOnlyWhenProcessIsResponding()
         {
             var methodCallCount = 0;
             var mockWebClient = new Mock<ITrexWebClient>();
@@ -171,10 +192,58 @@ namespace CollierTests.Mining
 
             var miner = new TrexMiner(logger.Object, Options.Create(settings), mockWebClient.Object, factory.Object, mockLogListener.Object, mockLogObserver.Object);
 
-            miner.Start();
-            miner.Start();
+            await miner.Start();
+            await miner.Start();
 
             methodCallCount.Should().Be(0, "we should only invoke the web client pause method when the process is running.");
+        }
+
+        [Fact]
+        public async void StartupDelayProcessingWorks()
+        {
+            var methodCallCount = 0;
+            var mockWebClient = new Mock<ITrexWebClient>();
+            mockWebClient.Setup(x => x.IsRunningAsync()).ReturnsAsync(() =>
+            {
+                methodCallCount++;
+                if (methodCallCount <= 1)
+                    return false;
+                return true;
+            });
+            var logger = new Mock<ILogger<TrexMiner>>();
+            var settings = new TrexMiner.Settings();
+            settings.StartupDelayAttempts = 2;
+            settings.StartupDelayInMs = 1;
+
+            var factory = new Mock<IMinerProcessFactory>();
+            var process = new Mock<IProcess>();
+
+            var spawned = false;
+
+            factory.Setup(x => x.GetNewOrExistingProcessAsync()).ReturnsAsync(() =>
+            {
+                spawned = true;
+                return process.Object;
+            });
+
+            factory.Setup(x => x.CurrentProcess).Returns(() =>
+            {
+                if (spawned)
+                    return process.Object;
+                return null;
+            });
+            process.Setup(x => x.HasExited).Returns(false);
+
+
+
+            var mockLogListener = new Mock<IMinerLogListener>();
+            var mockLogObserver = new Mock<IInternalLoggingFrameworkObserver>();
+
+            var miner = new TrexMiner(logger.Object, Options.Create(settings), mockWebClient.Object, factory.Object, mockLogListener.Object, mockLogObserver.Object);
+
+            await miner.Start();
+
+            mockWebClient.Verify(x => x.IsRunningAsync(), Times.Exactly(2), "we needed to loop at least twice before startup completed.");
         }
     }
 }
